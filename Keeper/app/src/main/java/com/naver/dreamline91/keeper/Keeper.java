4c1840b2e60a0ff53fd4eb97ad4879166bafe8ab
dreamline91 <dreamline91@naver.com>
2017-09-19 22:14:48 +0900

    modify use time

--- Keeper/app/src/main/java/com/naver/dreamline91/keeper/Keeper.java
+++ Keeper/app/src/main/java/com/naver/dreamline91/keeper/Keeper.java
@@ -28,7 +28,7 @@ public class Keeper extends Activity {
     private TextView textDate, textRandom, textBattery, textUsage;
     private ProgressBar progressBattery;
 
-    private int year, month, day, today, total;
+    private int year, month, day, today, total, now, update;
     /*나중에 커스텀으로 수정*/
     String random[] = {"dreamline91@naver.com로 연락주시면 사례하겠습니다"};
     /*나중에 커스텀으로 수정*/
@@ -57,6 +57,7 @@ public class Keeper extends Activity {
         year = cal.get(Calendar.YEAR);
         month = cal.get(Calendar.MONTH) + 1;
         day = cal.get(Calendar.DATE);
+        now = year*10000+month*100+day;
         textDate.setText(String.format("%4d.%d.%d.%s", year, month, day, week[(cal.get(Calendar.DAY_OF_WEEK) + 5) % 7]));
 
         textRandom.setText(random[(int) (Math.random() * random.length)]);
@@ -65,30 +66,33 @@ public class Keeper extends Activity {
         SharedPreferences.Editor editor = preferences.edit();
         today = preferences.getInt("keeper_today", 0);
         total = preferences.getInt("keeper_total", 0);
-        int savedYear = preferences.getInt("keeper_year", 0);
-        int savedMonth = preferences.getInt("keeper_month", 0);
-        int savedDay = preferences.getInt("keeper_day", 0);
-        if( savedYear == year && savedMonth == month && savedDay == day) {
-            if(preferences.getBoolean("keeper_use",false) == true) {
-                today += cal.get(Calendar.HOUR_OF_DAY) * 3600 + cal.get(Calendar.MINUTE) * 60 + cal.get(Calendar.SECOND);
-                today -= preferences.getInt("keeper_hour", 0) * 3600 + preferences.getInt("keeper_min", 0) * 60 + preferences.getInt("keeper_sec", 0);
-                editor.putInt("keeper_today", today);
-                editor.putBoolean("keeper_use",false);
-            }
-        }else if(!(savedYear == 0 && savedMonth == 0 && savedDay == 0)) {
-            if(preferences.getBoolean("keeper_use",false) == true) {
-                today += 24 * 3600 - preferences.getInt("keeper_hour", 0) * 3600 + preferences.getInt("keeper_min", 0) * 60 + preferences.getInt("keeper_sec", 0);
-                if (total != 0)
-                    total = (int) (today * 0.2f + total * 0.8f);
-                else
-                    total = today;
+        update = preferences.getInt("keeper_update", 0);
+        if(update == 0)
+            update = now;
+        else{
+            if(now == update){
+                if(preferences.getBoolean("keeper_use",false) == true) {
+                    today += cal.get(Calendar.HOUR_OF_DAY) * 3600 + cal.get(Calendar.MINUTE) * 60 + cal.get(Calendar.SECOND);
+                    today -= preferences.getInt("keeper_hour", 0) * 3600 + preferences.getInt("keeper_min", 0) * 60 + preferences.getInt("keeper_sec", 0);
+                    editor.putInt("keeper_today", today);
+                    editor.putBoolean("keeper_use",false);
+                }
+            }else{
+                if(preferences.getBoolean("keeper_use",false) == true) {
+                    today += 24 * 3600 - preferences.getInt("keeper_hour", 0) * 3600 + preferences.getInt("keeper_min", 0) * 60 + preferences.getInt("keeper_sec", 0);
+                    if (total != 0)
+                        total = (int) (today * 0.2f + total * 0.8f);
+                    else
+                        total = today;
                 /* 밑의 today 위치 변경하면 안됨 */
-                today = cal.get(Calendar.HOUR)*3600+cal.get(Calendar.MINUTE)*60+cal.get(Calendar.SECOND);
-                editor.putInt("keeper_total", total);
-                editor.putInt("keeper_today", today);
-                editor.putBoolean("keeper_use",false);
+                    today = cal.get(Calendar.HOUR)*3600+cal.get(Calendar.MINUTE)*60+cal.get(Calendar.SECOND);
+                    editor.putInt("keeper_total", total);
+                    editor.putInt("keeper_today", today);
+                    editor.putBoolean("keeper_use",false);
+                }
             }
         }
+        editor.putInt("keeper_update",update);
         editor.commit();
 
         textUsage.setText("핸드폰 "+(today/60)/60+"시간 "+(today/60)%60+"분 사용 (가중치 평균 : "+(total/60)/60+"시간 "+(total/60)%60+"분)");
@@ -162,10 +166,8 @@ public class Keeper extends Activity {
         Calendar cal = Calendar.getInstance();
         SharedPreferences preferences = getSharedPreferences("dreamline91", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        int savedYear = preferences.getInt("keeper_year", 0);
-        int savedMonth = preferences.getInt("keeper_month", 0);
-        int savedDay = preferences.getInt("keeper_day", 0);
-        if(!(savedYear == year && savedMonth == month && savedDay == day) && !(savedYear == 0 && savedMonth == 0 && savedDay == 0)) {
+        int update = preferences.getInt("keeper_update",0);
+        if((now != update) && now != 0) {
             today += 24 * 3600 - preferences.getInt("keeper_hour", 0) * 3600 + preferences.getInt("keeper_min", 0) * 60 + preferences.getInt("keeper_sec", 0);
             if (total != 0)
                 total = (int) (today * 0.2f + total * 0.8f);
@@ -176,9 +178,7 @@ public class Keeper extends Activity {
             editor.putInt("keeper_total", total);
             editor.putInt("keeper_today", today);
         }
-        editor.putInt("keeper_year",cal.get(Calendar.YEAR));
-        editor.putInt("keeper_month",cal.get(Calendar.MONTH) + 1);
-        editor.putInt("keeper_day",cal.get(Calendar.DATE));
+        editor.putInt("keeper_update",update);
         editor.putInt("keeper_hour",cal.get(Calendar.HOUR_OF_DAY));
         editor.putInt("keeper_min",cal.get(Calendar.MINUTE));
         editor.putInt("keeper_sec",cal.get(Calendar.SECOND));
